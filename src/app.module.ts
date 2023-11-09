4476bcc6d263a1d5045d8aac243b7f97052a05f0
esoft41 <rodrigosorlandini@gmail.com>
2023-11-09 20:05:17 -0300

    FEAT: Environment variables validation

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,19 @@
 import { Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
+
+import { envSchema } from "./env";
 
 import { PrismaService } from "./prisma/prisma.service";
 
 import { CreateAccountController } from "./controllers/create-account.controller";
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      validate: (env) => envSchema.parse(env),
+      isGlobal: true,
+    }),
+  ],
   controllers: [CreateAccountController],
   providers: [PrismaService],
 })
