f7deea593b05216bc6f1557893c7083fd2bde33a
esoft41 <rodrigosorlandini@gmail.com>
2023-11-10 00:27:18 -0300

    FEAT: Test databases isolated

---
+++ test/setup-e2e.ts
@@ -0,0 +1,33 @@
+import { execSync } from "node:child_process";
+import { randomUUID } from "node:crypto";
+import { PrismaClient } from "@prisma/client";
+import "dotenv/config";
+
+const prisma = new PrismaClient();
+
+const generateUniqueDatabaseURL = (schemaId: string) => {
+  if (!process.env.DATABASE_URL) {
+    throw new Error("DATABASE_URL env variable was not found.");
+  }
+
+  const url = new URL(process.env.DATABASE_URL);
+
+  url.searchParams.set("schema", schemaId);
+
+  return url.toString();
+};
+
+const schemaId = randomUUID();
+
+beforeAll(async () => {
+  const databaseURL = generateUniqueDatabaseURL(schemaId);
+
+  process.env.DATABASE_URL = databaseURL;
+
+  execSync("npx prisma migrate deploy");
+});
+
+afterAll(async () => {
+  await prisma.$executeRawUnsafe(`DROP SCHEMA IF EXISTS "${schemaId}" CASCADE`);
+  await prisma.$disconnect();
+});
