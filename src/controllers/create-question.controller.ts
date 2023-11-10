1098ba12c0ea7b84359d2366d8c8622306e712c9
esoft41 <rodrigosorlandini@gmail.com>
2023-11-10 00:16:57 -0300

    FEAT: Vitest and alias paths config

--- src/controllers/create-question.controller.ts
+++ src/controllers/create-question.controller.ts
@@ -1,12 +1,12 @@
 import { Body, Controller, HttpCode, Post, UseGuards } from "@nestjs/common";
 import { z } from "zod";
 
-import { CurrentUser } from "src/auth/current-user.decorator";
-import { JwtAuthGuard } from "src/auth/jwt-auth.guard";
-import { UserPayload } from "src/auth/jwt.strategy";
+import { CurrentUser } from "@/auth/current-user.decorator";
+import { JwtAuthGuard } from "@/auth/jwt-auth.guard";
+import { UserPayload } from "@/auth/jwt.strategy";
 
-import { PrismaService } from "src/prisma/prisma.service";
-import { ZodValidationPipe } from "src/pipes/zod-validation-pipe";
+import { PrismaService } from "@/prisma/prisma.service";
+import { ZodValidationPipe } from "@/pipes/zod-validation-pipe";
 
 const createQuestionBodySchema = z.object({
   title: z.string(),
--- src/controllers/fetch-recent-questions.controller.ts
+++ src/controllers/fetch-recent-questions.controller.ts
@@ -1,10 +1,10 @@
 import { Controller, Get, Query, UseGuards } from "@nestjs/common";
 import { z } from "zod";
 
-import { JwtAuthGuard } from "src/auth/jwt-auth.guard";
-import { ZodValidationPipe } from "src/pipes/zod-validation-pipe";
+import { JwtAuthGuard } from "@/auth/jwt-auth.guard";
+import { ZodValidationPipe } from "@/pipes/zod-validation-pipe";
 
-import { PrismaService } from "src/prisma/prisma.service";
+import { PrismaService } from "@/prisma/prisma.service";
 
 const pageQueryParamSchema = z
   .string()
