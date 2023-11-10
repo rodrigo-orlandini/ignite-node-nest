9423ef75c0d2476071a4138dbce349ab16e24410
esoft41 <rodrigosorlandini@gmail.com>
2023-11-09 23:59:51 -0300

    FEAT: Fetch recent questions controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,6 +8,7 @@ import { PrismaService } from "./prisma/prisma.service";
 import { CreateAccountController } from "./controllers/create-account.controller";
 import { AuthenticateController } from "./controllers/authenticate.controller";
 import { CreateQuestionController } from "./controllers/create-question.controller";
+import { FetchRecentQuestionsController } from "./controllers/fetch-recent-questions.controller";
 
 import { envSchema } from "./env";
 
@@ -23,6 +24,7 @@ import { envSchema } from "./env";
     CreateAccountController,
     AuthenticateController,
     CreateQuestionController,
+    FetchRecentQuestionsController,
   ],
   providers: [PrismaService],
 })
